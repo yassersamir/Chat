5bfac6d40b357624a6c8a494d4a9f018976a50f9
yasersamirragab@gmail.com <yassersamir@email.com>
2019-11-25 15:39:06 +0200

    add ssl security

--- chat/src/main/java/chatSE/Client.java
+++ chat/src/main/java/chatSE/Client.java
@@ -3,10 +3,13 @@ package chatSE;
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.SSLParameters;
 
 public class Client {
 	final static int ServerPort = 1234;
-	
 
 	public static void main(String args[]) throws UnknownHostException, IOException {
 		@SuppressWarnings("resource")
@@ -17,30 +20,33 @@ public class Client {
 		InetAddress ip = InetAddress.getByName("localhost");
 		// check user authentication
 		System.out.println("enter your name : ");
-		 String username = bf.readLine();
+		String username = bf.readLine();
 		System.out.println("enter your password : ");
 		String password = bf.readLine();
 
 		if (mf.readUsers(username, password)) {
-			
-			// establish the connection
-			@SuppressWarnings("resource")
-			Socket s = new Socket(ip, ServerPort);
-			
+
+			SocketFactory factory = SSLSocketFactory.getDefault();
+
+			Socket connection = factory.createSocket(ip, ServerPort);
+			((SSLSocket) connection).setEnabledCipherSuites(new String[] { "TLS_DHE_DSS_WITH_AES_256_CBC_SHA256" });
+			((SSLSocket) connection).setEnabledProtocols(new String[] { "TLSv1.2" });
+			SSLParameters sslParams = new SSLParameters();
+			sslParams.setEndpointIdentificationAlgorithm("HTTPS");
+			((SSLSocket) connection).setSSLParameters(sslParams);
 			// obtaining input and out streams
-			DataInputStream dis = new DataInputStream(s.getInputStream());
-			DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-			
+			DataInputStream dis = new DataInputStream(connection.getInputStream());
+			DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
 
 			// sendMessage thread
 			Thread sendMessage = new Thread(() -> {
-				
+
 				while (true) {
 					String msg = scn.nextLine();
 					try {
 						dos.writeUTF(msg);
 					} catch (IOException e) {
-						return;
+						e.printStackTrace();
 					}
 				}
 			});
@@ -52,12 +58,13 @@ public class Client {
 						String msg = dis.readUTF();
 						System.out.println(msg);
 					} catch (IOException e) {
-						return;
+						e.printStackTrace();
 					}
 				}
 			});
 			sendMessage.start();
 			readMessage.start();
 		}
+
 	}
 }
--- chat/src/main/java/chatSE/Server.java
+++ chat/src/main/java/chatSE/Server.java
@@ -3,6 +3,9 @@ package chatSE;
 import java.io.*;
 import java.util.*;
 import java.net.*;
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
 
 public class Server {
 
@@ -13,27 +16,29 @@ public class Server {
 
 		ManageFile mf = new ManageFile();
 		mf.writeUsers();
-		@SuppressWarnings("resource")
-		ServerSocket server = new ServerSocket(1234);
-		Socket socket;
-		// getting client request
-		while (true) {
-			socket = server.accept();
 
+		ServerSocketFactory factory = SSLServerSocketFactory.getDefault();
+		ServerSocket listener = factory.createServerSocket(1234);
+		((SSLServerSocket) listener).setNeedClientAuth(true);
+		((SSLServerSocket) listener).setEnabledCipherSuites(new String[] { "TLS_DHE_DSS_WITH_AES_256_CBC_SHA256" });
+		((SSLServerSocket) listener).setEnabledProtocols(new String[] { "TLSv1.2" });
+		while (true) {
+			Socket socket = listener.accept();
 			// obtain input and output streams
 			DataInputStream dis = new DataInputStream(socket.getInputStream());
 			DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
 
 			// Create a new handler object for handling this request.
 
-			ClientHandler mtch = new ClientHandler(socket ,dis, "client"+i, dos);
+			ClientHandler mtch = new ClientHandler(socket, dis, "client" + i, dos);
 			Thread t = new Thread(mtch);
 			System.out.println("new client adding ...");
 			ar.add(mtch);
 			t.start();
-			
+
 			i++;
+
 		}
+
 	}
 }
-
